ad77aa881e4ba06c62c523f68f34b785cf5d73c0
cheroo <chinonnnonnon@gmail.com>
2017-06-26 18:05:22 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,4 +1,23 @@
+for(var i=1;i<100;i++){
+  var li=document.createElement('li');
+  if(i%3==0){
+    if(i%5==0){
+        li.textContent=FizzBuzz;
+    }
+    else{
+         li.textContent=Fizz;
+    }
+  else if(i%5==0){
+            li.textContent=Buzz;
+  }
+    else{
+        li.textContent=i;
+    }
+  }
+    document.createElementById('list').appendChild(li);
+}
 
+  
 
 document.getElementById('from').onsubmit=function(){
   window.alert(document.getElementById('from').word.value);
