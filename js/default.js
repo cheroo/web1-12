2aa8e20d8f63ed47e14bd096b662b4b400e0fabb
cheroo <chinonnnonnon@gmail.com>
2017-06-26 17:37:58 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,6 +1,4 @@
-var li=document.createElement('li');
-li.textContent='アイテム';
-document.getElementById('list').appendChild(li);
+
 
 document.getElementById('from').onsubmit=function(){
   window.alert(document.getElementById('from').word.value);
